0f14427efacf5cdb012397a649ef2ab5a886d31c
Dave Russell <forfuncsake@gmail.com>
2018-08-09 00:11:26 +1000

    Refactor to allow config via envconfig and flags

---
+++ cmd/gdhk/garagedoor.go
@@ -0,0 +1,167 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/brutella/hc/accessory"
+	"github.com/brutella/hc/characteristic"
+	"github.com/brutella/hc/service"
+)
+
+// This is a value specified in the ESP8266 firmware
+// to trigger an explicit button press, regardless
+// of current door state.
+const press int = -5
+
+var stateURL = map[int]string{
+	characteristic.TargetDoorStateOpen:   "/open",
+	characteristic.TargetDoorStateClosed: "/close",
+	press: "/press",
+}
+
+// GarageDoor represents a HomeKit Accessory with a GarageDoorOpener
+// and a Switch. The Opener will intelligently request a target state
+// for the door (opened/closed), where the switch will always
+// trigger the door button.
+type GarageDoor struct {
+	URL      string
+	User     string
+	Password string
+
+	*accessory.Accessory
+	Opener *service.GarageDoorOpener
+	Button *service.Switch
+
+	state int
+	guard chan struct{}
+}
+
+// NewGarageDoor returns a GarageDoor with the provided accessory info.
+func NewGarageDoor(conf Config, info accessory.Info) *GarageDoor {
+	acc := GarageDoor{
+		URL:       conf.URL,
+		User:      conf.Username,
+		Password:  conf.Password,
+		Accessory: accessory.New(info, accessory.TypeGarageDoorOpener),
+		Button:    service.NewSwitch(),
+		Opener:    service.NewGarageDoorOpener(),
+		guard:     make(chan struct{}, 1),
+	}
+	acc.AddService(acc.Opener.Service)
+	acc.AddService(acc.Button.Service)
+
+	acc.Opener.CurrentDoorState.OnValueRemoteGet(acc.getState)
+	acc.Opener.TargetDoorState.OnValueRemoteGet(acc.getTargetState)
+	acc.Opener.TargetDoorState.OnValueRemoteUpdate(acc.setState)
+	acc.Opener.CurrentDoorState.SetEventsEnabled(true)
+	acc.Button.On.OnValueRemoteUpdate(acc.pressButton)
+
+	// Load a token into the guard channel
+	acc.guard <- struct{}{}
+
+	return &acc
+}
+
+func (d *GarageDoor) pressButton(on bool) {
+	if on {
+		d.setState(press)
+
+		// Always switch back to "off" to act like the
+		// momentary switch this represents
+		d.Button.On.SetValue(false)
+	}
+}
+
+func (d *GarageDoor) setState(to int) {
+	log.Printf("setState called")
+
+	path, ok := stateURL[to]
+	if !ok {
+		log.Printf("unsupported state ID requested: %d", to)
+		return
+	}
+
+	req, err := http.NewRequest(http.MethodPost, d.URL+path, nil)
+	if err != nil {
+		log.Printf("failed to create POST request: %v", err)
+		return
+	}
+
+	req.SetBasicAuth(d.User, d.Password)
+	_, err = http.DefaultClient.Do(req)
+	if err != nil {
+		log.Printf("failed to post to button: %v", err)
+	}
+}
+
+func (d *GarageDoor) getState() (state int) {
+	defer func() {
+		if state >= 0 {
+			d.state = state
+		} else {
+			state = characteristic.CurrentDoorStateStopped
+		}
+	}()
+	state = -1
+
+	// Guard the module from being probed more than once a second
+	// by returning the last saved state
+	select {
+	case <-d.guard:
+		go func() {
+			<-time.After(1 * time.Second)
+			d.guard <- struct{}{}
+		}()
+	default:
+		log.Println("Guarding probe from excessive polls")
+		return d.state
+	}
+
+	resp, err := http.Get(d.URL)
+	if err != nil {
+		log.Printf("error getting status: %v\n", err)
+		return state
+	}
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Printf("error reading status response: %v\n", err)
+		return state
+	}
+
+	var a struct {
+		Success bool   `json:"success"`
+		Status  int    `json:"status"`
+		Message string `json:"message"`
+	}
+	err = json.Unmarshal(b, &a)
+	if err != nil {
+		log.Printf("error marshalling status response: %v\n", err)
+		return state
+	}
+
+	if !a.Success {
+		log.Printf("got error from API: %s\n", a.Message)
+		if a.Status < 1 {
+			return state
+		}
+	}
+
+	state = a.Status
+	return state
+}
+
+func (d *GarageDoor) getTargetState() (state int) {
+	switch d.getState() {
+	case characteristic.CurrentDoorStateClosed, characteristic.CurrentDoorStateClosing:
+		return characteristic.TargetDoorStateClosed
+	case characteristic.CurrentDoorStateOpen, characteristic.CurrentDoorStateOpening:
+		return characteristic.TargetDoorStateOpen
+	default:
+		return characteristic.CurrentDoorStateStopped
+	}
+}
--- cmd/gdhk/main.go
+++ cmd/gdhk/main.go
@@ -1,183 +1,117 @@
 package main
 
 import (
-	"encoding/json"
-	"io/ioutil"
-	"log"
+	"flag"
+	"fmt"
 	"net/http"
+	"net/url"
+	"os"
 	"time"
 
 	"github.com/brutella/hc"
 	"github.com/brutella/hc/accessory"
-	"github.com/brutella/hc/characteristic"
-	"github.com/brutella/hc/service"
+	"github.com/kelseyhightower/envconfig"
 )
 
-// CHANGE ME
-const httpuser = "admin"
-const httppass = "password"
-const doorAPI = "192.168.0.100"
+// Config is used as a value store for envconfig
+// (configuration via environment variables)
+type Config struct {
+	URL    string
+	Listen uint `default:"8180"`
 
-type GarageDoor struct {
-	*accessory.Accessory
-	Opener *service.GarageDoorOpener
-	Button *service.Switch
-	state  int
-	guard  chan struct{}
-}
-
-func NewGarageDoor(info accessory.Info) *GarageDoor {
-	acc := GarageDoor{
-		Accessory: accessory.New(info, accessory.TypeGarageDoorOpener),
-		Button:    service.NewSwitch(),
-		Opener:    service.NewGarageDoorOpener(),
-		guard:     make(chan struct{}, 1),
-	}
-	acc.AddService(acc.Opener.Service)
-	acc.AddService(acc.Button.Service)
-
-	acc.Opener.CurrentDoorState.OnValueRemoteGet(acc.getState)
-	acc.Opener.TargetDoorState.OnValueRemoteGet(acc.getTargetState)
-	acc.Opener.TargetDoorState.OnValueRemoteUpdate(acc.setState)
-	acc.Opener.CurrentDoorState.SetEventsEnabled(true)
-	acc.Button.On.OnValueRemoteUpdate(acc.pressButton)
-
-	// Load a token into the guard channel
-	acc.guard <- struct{}{}
-
-	return &acc
+	Name        string `default:"GarageDoor"`
+	Serial      string `default:"GDOOR-0001"`
+	PIN         string `default:"12344321"`
+	StoragePath string
+	Username    string `default:"admin"`
+	Password    string `default:"password"`
 }
 
 func main() {
+	conf := Config{}
+	err := envconfig.Process("gd", &conf)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to read config from environment: %v\n", err)
+		os.Exit(1)
+	}
+
+	flag.StringVar(&conf.URL, "url", conf.URL, "URL for the garage door API")
+	flag.UintVar(&conf.Listen, "listen", conf.Listen, "TCP port for HTTP listener of this proxy")
+	flag.StringVar(&conf.Name, "name", conf.Name, "Name of the HomeKit accessory")
+	flag.StringVar(&conf.Serial, "serial", conf.Serial, "Serial number override")
+	flag.StringVar(&conf.PIN, "pin", conf.PIN, "HomeKit setup code/PIN for this accessory")
+	flag.StringVar(&conf.StoragePath, "path", conf.StoragePath, "Storage path for HomeKit pairing database")
+	flag.StringVar(&conf.Username, "u", conf.Username, "`username` for requests to garage door API")
+	flag.StringVar(&conf.Password, "p", conf.Password, "`password` for requests to garage door API")
+	e := flag.Bool("e", false, "show envconfig help and exit")
+	flag.Parse()
+
+	if *e {
+		envconfig.Usage("gd", &Config{})
+		os.Exit(0)
+	}
+
+	if conf.URL == "" {
+		fmt.Fprintln(os.Stderr, "URL for garage door must be specified")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	u, err := url.Parse(conf.URL)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "URL value is invalid: %q\n", conf.URL)
+		os.Exit(1)
+	}
+	if u.Scheme == "" {
+		u.Scheme = "http"
+	}
+	conf.URL = u.String()
+
+	if conf.Listen == 0 {
+		fmt.Fprintln(os.Stderr, "Listen port must be specified (non-zero)")
+		flag.Usage()
+		os.Exit(1)
+	}
+
 	info := accessory.Info{
-		Name:         "GarageDoor",
-		SerialNumber: "GDOOR-0001",
+		Name:         conf.Name,
+		SerialNumber: conf.Serial,
 		Manufacturer: "forfuncsake",
 		Model:        "GDHK",
 	}
-	door := NewGarageDoor(info)
+	door := NewGarageDoor(conf, info)
 
-	config := hc.Config{Pin: "12344321", StoragePath: "/usr/local/garagedoor/var/db"}
+	// "/usr/local/garagedoor/var/db"
+	config := hc.Config{Pin: conf.PIN, StoragePath: conf.StoragePath}
 	t, err := hc.NewIPTransport(config, door.Accessory)
 	if err != nil {
-		log.Panic(err)
+		fmt.Fprintf(os.Stderr, "could not create IP transport: %v\n", err)
+		os.Exit(1)
 	}
 
 	hc.OnTermination(func() {
 		<-t.Stop()
 	})
 
-	http.HandleFunc("/refresh", func(http.ResponseWriter, *http.Request) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/refresh", func(http.ResponseWriter, *http.Request) {
 		door.getState()
 		door.Opener.CurrentDoorState.SetValue(door.state)
 		door.Opener.TargetDoorState.SetValue(door.getTargetState())
 	})
 
+	timeout := 5 * time.Second
+	srv := http.Server{
+		Addr:         fmt.Sprintf(":%d", conf.Listen),
+		ReadTimeout:  timeout,
+		WriteTimeout: timeout,
+		IdleTimeout:  timeout,
+		Handler:      mux,
+	}
+
 	go func() {
-		http.ListenAndServe(":8180", nil)
+		srv.ListenAndServe()
 	}()
 
-	log.Println("Starting...")
 	t.Start()
-}
-
-func (d *GarageDoor) getTargetState() (state int) {
-	switch d.getState() {
-	case characteristic.CurrentDoorStateClosed, characteristic.CurrentDoorStateClosing:
-		return characteristic.TargetDoorStateClosed
-	case characteristic.CurrentDoorStateOpen, characteristic.CurrentDoorStateOpening:
-		return characteristic.TargetDoorStateOpen
-	default:
-		return characteristic.CurrentDoorStateStopped
-	}
-}
-
-const press int = -5
-
-var stateURL = map[int]string{
-	characteristic.TargetDoorStateOpen:   "/open",
-	characteristic.TargetDoorStateClosed: "/close",
-	press: "/press",
-}
-
-func (d *GarageDoor) pressButton(bool) {
-	d.setState(press)
-}
-
-func (d *GarageDoor) setState(to int) {
-	log.Printf("setState called\n")
-
-	path, _ := stateURL[to]
-
-	req, err := http.NewRequest(http.MethodPost, "http://"+doorAPI+path, nil)
-	if err != nil {
-		log.Printf("failed to create POST request: %v", err)
-		return
-	}
-
-	req.SetBasicAuth(httpuser, httppass)
-	_, err = http.DefaultClient.Do(req)
-	if err != nil {
-		log.Printf("failed to post to button: %v", err)
-	}
-}
-
-func (d *GarageDoor) getState() (state int) {
-	log.Printf("getState called\n")
-	defer func() {
-		if state >= 0 {
-			d.state = state
-		} else {
-			state = characteristic.CurrentDoorStateStopped
-		}
-	}()
-	state = -1
-
-	// Guard the module from being probed more than once a second
-	// by returning the last saved state
-	select {
-	case <-d.guard:
-		go func() {
-			<-time.After(1 * time.Second)
-			d.guard <- struct{}{}
-		}()
-	default:
-		log.Println("Guarding probe from excessive polls")
-		return d.state
-	}
-
-	resp, err := http.Get("http://" + doorAPI)
-	if err != nil {
-		log.Printf("error getting status: %v\n", err)
-		return state
-	}
-
-	b, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Printf("error reading status response: %v\n", err)
-		return state
-	}
-
-	var a struct {
-		Success bool   `json:"success"`
-		Status  int    `json:"status"`
-		Message string `json:"message"`
-	}
-	err = json.Unmarshal(b, &a)
-	if err != nil {
-		log.Printf("error marshalling status response: %v\n", err)
-		return state
-	}
-
-	if !a.Success {
-		log.Printf("got error from API: %s\n", a.Message)
-		if a.Status < 1 {
-			return state
-		}
-	}
-
-	log.Printf("returning %d", a.Status)
-	state = a.Status
-
-	return state
 }
